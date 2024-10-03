2f0d8a529c60785ec8419977c85adabc2c9f5815
Sahar Dembus <demshr8307@gmail.com>
2024-10-03 14:41:33 +0300

    changed to app class, and added 2 ctkframes

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
+from tkinter.tix import COLUMN
+
 import customtkinter
+import tkintermapview
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()
-app.geometry("800x480")
 
-app.mainloop()
+class App(customtkinter.CTk):
+    APP_NAME = "RedAlertMapViewer"
+    WIDTH = 800
+    HEIGHT = 500
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title = App.APP_NAME
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.minsize(App.WIDTH, App.HEIGHT)
+
+        self.market_list = []
+
+        # ============ create two CTkFrames ============
+
+        self.grid_columnconfigure(0, weight=0)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.left_frame = customtkinter.CTkFrame(master=self, width=150, corner_radius=0, fg_color=None)
+        self.left_frame.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+
+        self.right_frame = customtkinter.CTkFrame(master=self, corner_radius=0, fg_color=None)
+        self.right_frame.grid(row=0, column=1, rowspan=1, padx=0, pady=0, sticky="nsew")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
