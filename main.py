2e8c4f8f59628ffb9879513e760df26b3188c4b7
Sahar Dembus <demshr8307@gmail.com>
2024-10-03 14:20:40 +0300

    Added App window

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()
+app.geometry("800x480")
+
+app.mainloop()
